d23d9e471b69912cba3ece29b1f3fad5ad83b40f
Franziska Otto <franziska.otto@gmx.at>
2024-02-03 21:53:19 +0100

    added backend, added frontend, css still missing

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 
 const express = require('express');
 const path = require('path');
+const cors = require("cors");
 
 
 
 const server = express();
-server.use(express.json());
+server.use(express.json())
+server.use(cors());
 
 
-server.use(express.static(path.join(__dirname, "client", "public")));
 
+// server.use(express.static("client"));
+
+server.use(
+  express.static("client", {
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith(".css")) {
+        res.set("Content-Type", "text/css");
+      }
+      if (path.endsWith(".js")) {
+        res.set("Content-Type", "text/javascript");
+      }
+    },
+  })
+);
 
 const PORT = 8080;
+
 
 server.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
@@ -20,9 +36,19 @@ server.use((req, res, next) => {
 
 // Handle requests to the root URL
 server.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(path.join(__dirname + "../../client/index.html"));
+});
+
+server.get("/script.js", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../client/script.js"));
+});
+
+server.get("/style.css", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../client/style.css"));
 });
 
 server.listen(PORT, () => {
-  console.log(`server is listening on Port http://localhost:${PORT}`);
+  console.log(`server is listening on Port http://127.0.0.1:${PORT}/`);
 })
+
+
